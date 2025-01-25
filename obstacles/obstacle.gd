6cb534392b6825e5ed76dec030762ecedc78bbdc
Connor Haynes <theconnorhaynes@gmail.com>
2025-01-25 16:21:02 -0500

    hides spike ball when it is hit

--- obstacles/obstacle.gd
+++ obstacles/obstacle.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+@export var sprite : NodePath
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,5 +23,7 @@ func _on_rigid_body_2d_body_entered(body: Node) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if(body.has_method("pop")):
 		body.pop()
+		hide()
+		pass
 	print("entered")
 	pass # Replace with function body.
