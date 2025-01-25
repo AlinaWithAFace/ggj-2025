0a9a133f30466cabb1a59050da5cdd5e951a369e
Zachary Pavlov <zpavlov@schellgames.local>
2025-01-24 23:23:18 -0500

    elizabeth's bubbles and gradients

--- Root.gd
+++ Root.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	var spriteY = 400 - distance;
+	var spriteY = 1000 - distance;
 	speed += accel * delta
 	distance += speed * delta
 	print(distance)
